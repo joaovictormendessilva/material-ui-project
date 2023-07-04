ee6b693e3b1dfce6b102e3e97b57a1848d946f78
João Victor <joao_victor-08@hotmail.com>
2023-07-04 16:38:43 -0300

    feat: Add validação no campo

--- src/shared/components/BoletoRapido/BoletoRapido.tsx
+++ src/shared/components/BoletoRapido/BoletoRapido.tsx
@@ -2,11 +2,49 @@ import { Typography, Box, TextField, Button } from '@mui/material'
 import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
 import KeyboardArrowLeftOutlinedIcon from '@mui/icons-material/KeyboardArrowLeftOutlined';
 
+import { ChangeEvent, useEffect, useState } from 'react';
+
 interface IBoletoRapidoProps {
   backToLogin: () => void
 }
 
+interface IBoletoRapidoFieldsProps {
+  cpf_cnpj: string
+}
+
 export function BoletoRapido({ backToLogin }: IBoletoRapidoProps) {
+
+  const [boletoRapidoFields, setBoletoRapidoFields] = useState<IBoletoRapidoFieldsProps>({} as IBoletoRapidoFieldsProps);
+  const [isEmptyCheck, setIsEmptyCheck] = useState({
+    cpf_cnpj: false
+  })
+
+  const handleChangeInputs = (e: ChangeEvent<HTMLInputElement>) => {
+    setBoletoRapidoFields((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const handleCheckSend = () => {
+    const isEmptyFields = {
+      cpf_cnpj: !boletoRapidoFields.cpf_cnpj
+    }
+
+    setIsEmptyCheck(isEmptyFields);
+
+    if (Object.values(isEmptyFields).every(field => !field)) {
+      console.log("Sucesso Boleto", boletoRapidoFields)
+    }
+    else {
+      console.log("Erro Boleto", boletoRapidoFields)
+    }
+  }
+
+  useEffect(() => {
+    boletoRapidoFields.cpf_cnpj && setIsEmptyCheck((prev) => ({ ...prev, cpf_cnpj: false }))
+  }, [boletoRapidoFields.cpf_cnpj])
+
   return (
     <>
       <Typography variant='h6' fontSize={14} component="label">
@@ -14,7 +52,15 @@ export function BoletoRapido({ backToLogin }: IBoletoRapidoProps) {
       </Typography>
 
       <Box>
-        <TextField size='small' fullWidth placeholder='CPF/CNPJ'></TextField>
+        <TextField
+          name='cpf_cnpj'
+          size='small'
+          fullWidth
+          placeholder='CPF/CNPJ'
+          onChange={handleChangeInputs}
+          error={isEmptyCheck.cpf_cnpj}
+          helperText={isEmptyCheck.cpf_cnpj && "Campo obrigatório."}
+        />
       </Box>
 
       <Box display="flex" justifyContent="space-between" marginTop={1} gap={1}>
@@ -29,7 +75,9 @@ export function BoletoRapido({ backToLogin }: IBoletoRapidoProps) {
         <Button
           fullWidth
           variant='contained'
-          endIcon={<ChevronRightOutlinedIcon />}>
+          endIcon={<ChevronRightOutlinedIcon />}
+          onClick={handleCheckSend}
+        >
           ACESSAR
         </Button>
       </Box>
